2373b1f0670ecc3115124e3352a735278b531427
Lucashenriquerocha <112584046+Lucashenriquerocha@users.noreply.github.com>
2024-03-26 19:52:57 -0300

    Calculadora - Passo 1

---
+++ calculadora/src/main/java/com/mycompany/calculadora/calculadora.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author lucas
+ */
+public class calculadora {
+    //Soma
+    public int somar(int a, int b){
+        return a + b;
+    }
+    //Subtração
+    public int subtracao(int a, int b){
+        return a - b;
+    }
+    //Multiplicação
+    public int multiplicacao(int a, int b){
+        return a * b;
+    }
+    //Divisão
+    public double divisao (double a, double b){
+        return a / b;
+    }
+    
+}
