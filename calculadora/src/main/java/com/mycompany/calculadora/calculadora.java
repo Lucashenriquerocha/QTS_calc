ee0b122078a809a38a8d33fc193438a45d12cf32
Lucashenriquerocha <112584046+Lucashenriquerocha@users.noreply.github.com>
2024-03-26 21:42:17 -0300

    Calculadora - Passo 3

--- calculadora/src/main/java/com/mycompany/calculadora/calculadora.java
+++ calculadora/src/main/java/com/mycompany/calculadora/calculadora.java
@@ -7,22 +7,46 @@ package com.mycompany.calculadora;
 /**
  *
  * @author lucas
- * 
+ * @version 1.2
+ * @since Release 1.2 da aplicação
  */
 public class calculadora {
-    
+    /**
+     * 
+     * @param a , primeiro valor utilizado para a soma
+     * @param b , segundo valor utilizado para a soma
+     * @return , resultado
+     */
     //Soma
     public int somar(int a, int b){
         return a + b;
     }
+     /**
+     * 
+     * @param a , primeiro valor utilizado para a subtração
+     * @param b , segundo valor utilizado para a subtração
+     * @return , resultado
+     */
     //Subtração
     public int subtracao(int a, int b){
         return a - b;
     }
+     /**
+     * 
+     * @param a , primeiro valor utilizado para a multiplicação
+     * @param b , segundo valor utilizado para a multiplicação
+     * @return , resultado
+     */
     //Multiplicação
     public int multiplicacao(int a, int b){
         return a * b;
     }
+     /**
+     * 
+     * @param a , primeiro valor utilizado para a divisão
+     * @param b , segundo valor utilizado para a divisão
+     * @return , resultado
+     */
     //Divisão
     public double divisao (double a, double b){
         return a / b;
